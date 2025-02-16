b173b08a23a85cc5c7b1cdb522fe3c84bd5c02df
Cesar Galvis Leon <cgalvisleon@gmail.com>
2025-02-16 09:16:55 -0500

    Backup

--- drivers/postgres/sql-insert.go
+++ drivers/postgres/sql-insert.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"github.com/cgalvisleon/et/et"
 	"github.com/cgalvisleon/et/strs"
 	"github.com/cgalvisleon/et/utility"
 	jdb "github.com/cgalvisleon/jdb/jdb"
@@ -10,49 +11,29 @@ func (s *Postgres) sqlInsert(command *jdb.Command) string {
 	from := command.From
 	columns := ""
 	values := ""
-	for i, value := range command.Values {
-		record := ""
-		for key, val := range value.Columns {
-			if i == 0 {
-				column := key
-				columns = strs.Append(columns, column, ", ")
+	atribs := et.Json{}
+	for _, value := range command.Values {
+		for key, fld := range value {
+			if fld.Column == from.SourceField || fld.Column == from.FullTextField {
+				continue
+			} else if fld.Column.TypeColumn == jdb.TpColumn {
+				columns = strs.Append(columns, key, ", ")
+				def := strs.Format(`%v`, utility.Quote(fld.Value))
+				values = strs.Append(values, def, ", ")
+			} else if fld.Column.TypeColumn == jdb.TpAtribute && from.SourceField != nil {
+				atribs.Set(key, fld.Value)
 			}
-
-			value := utility.Quote(val)
-			def := strs.Format(`%v`, value)
-			record = strs.Append(record, def, ", ")
 		}
-		if from.SourceField != nil && len(value.Atribs) > 0 {
-			if i == 0 {
-				column := from.SourceField.Name
-				columns = strs.Append(columns, column, ", ")
-			}
+		if from.SourceField != nil && len(atribs) > 0 {
+			column := from.SourceField.Name
+			columns = strs.Append(columns, column, ", ")
 
-			value := value.Atribs.ToString()
-			def := strs.Format(`'%v'::jsonb`, value)
-			record = strs.Append(record, def, ", ")
+			def := strs.Format(`'%v'::jsonb`, atribs.ToString())
+			values = strs.Append(values, def, ", ")
 		}
-		if from.FullTextField != nil {
-			if i == 0 {
-				column := from.FullTextField.Name
-				columns = strs.Append(columns, column, ", ")
-			}
-
-			tsvector := ""
-			for _, key := range from.FullTextField.FullText {
-				if value.Data[key] != nil {
-					val := utility.Quote(value.Data[key])
-					tsvector = strs.Append(tsvector, strs.Format(`%v`, val), " || '' || ")
-				}
-			}
-			def := strs.Format(`to_tsvector('%s', '%s')`, from.FullTextField.Language, value)
-			record = strs.Append(record, def, ", ")
-		}
-		def := strs.Format(`(%s)`, record)
-		values = strs.Append(values, def, "\n")
 	}
 
 	objects := s.sqlJsonObject(from)
-	result := "INSERT INTO %s(%s)\nVALUES %s\nRETURNING\njsonb_build_object(\n'before', jsonb_build_object(),\n'after', (%s)) AS result;"
+	result := "INSERT INTO %s(%s)\nVALUES (%s)\nRETURNING\njsonb_build_object(\n'before', jsonb_build_object(),\n'after', (%s)) AS result;"
 	return strs.Format(result, from.Table, columns, values, objects)
 }
